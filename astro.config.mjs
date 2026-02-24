4f2877038b231eb3930342ff985597a7f1a5e4d8
shirokuro(419087) <kazuki419087@gmail.com>
2026-02-24 16:37:33 +0900

    feat: github actionsによる自動デプロイのためのワークフローを追加

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: 'https://419087.github.io',
+    base: '/shirokuro-portfolio',
+});
