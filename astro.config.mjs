0da0ca6f9608a3cc420cb260b9ffbf9c6063e882
shirokuro(419087) <kazuki419087@gmail.com>
2026-02-25 11:58:49 +0900

    add: Tailwindを導入

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,14 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import tailwindcss from '@tailwindcss/vite';
+
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://419087.github.io',
-    base: '/shirokuro-portfolio',
+  site: 'https://419087.github.io',
+  base: '/shirokuro-portfolio',
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
