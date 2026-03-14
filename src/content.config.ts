e31603296b523c1525cda364fe316cba4875915a
shirokuro(419087) <kazuki419087@gmail.com>
2026-03-15 03:23:08 +0900

    feat: astroのバージョンを6.0.4にアップデート

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,10 +1,12 @@
 import { defineCollection, z } from 'astro:content';
+import { glob } from 'astro/loaders';
 
 const worksCollection = defineCollection({
-  type: 'content',
+  loader: glob({ pattern: '**/[^_]*.{md,mdx}', base: "./src/content/works" }),
+  
   schema: z.object({
     title: z.string(),
-    publishDate: z.date(),
+    publishDate: z.coerce.date(),
     description: z.string(),
     tags: z.array(z.string()),
     image: z.string().optional(),
